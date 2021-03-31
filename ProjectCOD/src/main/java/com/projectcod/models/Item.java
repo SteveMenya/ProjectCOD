54baa3fb6b282e62a19f731473d977e78a7a11a5
SteveOtieno14 <jjmenya14@gmail.com>
2021-03-31 13:07:33 -0500

    Added item class to model

---
+++ ProjectCOD/src/main/java/com/projectcod/models/Item.java
@@ -0,0 +1,69 @@
+package com.projectcod.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "items")
+public class Item {
+	@Id
+	private int itemId;
+	private String itemName;
+	private int itemCost;
+	private String description;
+	private String size;
+	
+	//default constructor
+	public Item() {
+		super();
+	}
+
+	public int getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public int getItemCost() {
+		return itemCost;
+	}
+
+	public void setItemCost(int itemCost) {
+		this.itemCost = itemCost;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [itemId=" + itemId + ", itemName=" + itemName + ", itemCost=" + itemCost + ", description="
+				+ description + ", size=" + size + "]";
+	}
+	
+	
+}
