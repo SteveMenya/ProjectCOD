762a3c3b9a2bbf64ce94682a414ce450a4e7098f
SteveOtieno14 <jjmenya14@gmail.com>
2021-04-09 16:57:56 -0500

    adding test package

---
+++ ProjectCOD/src/test/java/com/projectcod/test/CustomerServiceJUnitTest.java
@@ -0,0 +1,22 @@
+package com.projectcod.test;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.projectcod.repositories.CustomerRepository;
+import com.projectcod.service.CustomerService;
+
+public class CustomerServiceJUnitTest {
+	/*
+	 * private static CustomerService customerService; private static
+	 * CustomerRepository customerRepository;
+	 * 
+	 * @BeforeClass public static void setup() { customerRepository =
+	 * Mockito.mock(CustomerRepository.class); customerService = new
+	 * CustomerService(customerRepository); }
+	 * 
+	 * @Test public void testFindByEmail() {
+	 * 
+	 * }
+	 */
+}
