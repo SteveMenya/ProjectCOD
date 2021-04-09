947fd1b71a1f51d7aad01805773fa579331253b3
SteveOtieno14 <jjmenya14@gmail.com>
2021-04-08 23:08:29 -0500

    deleted cold item repo

--- ProjectCOD/src/main/java/com/projectcod/repositories/ColdItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projectcod.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.projectcod.models.ColdItem;
-
-public interface ColdItemRepository extends CrudRepository<ColdItem,String > {
-	public Iterable<ColdItem> findAll();
-}
