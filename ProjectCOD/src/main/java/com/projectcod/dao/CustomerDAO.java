d06605109c63bc5bae15b68f04b606d7ee5fd378
SteveOtieno14 <jjmenya14@gmail.com>
2021-04-09 09:41:50 -0500

    removing unnecessary dao package.

--- ProjectCOD/src/main/java/com/projectcod/dao/CustomerDAO.java
+++
@@ -1,9 +0,0 @@
-package com.projectcod.dao;
-
-import java.util.List;
-
-import com.projectcod.models.Customer;
-
-public interface CustomerDAO {
-	public List<Customer> getAllCustomers();
-}
--- ProjectCOD/src/main/java/com/projectcod/dao/OrderDAO.java
+++
@@ -1,7 +0,0 @@
-package com.projectcod.dao;
-
-import com.projectcod.models.Order;
-
-public interface OrderDAO {
-		public Order getOrderByCustomerEmail();
-}
