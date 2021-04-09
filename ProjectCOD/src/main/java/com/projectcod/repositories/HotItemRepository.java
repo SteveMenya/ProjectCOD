1aab459d1e5820d312e1d6465c5ea4277862767e
SteveOtieno14 <jjmenya14@gmail.com>
2021-04-08 23:08:43 -0500

    deleted hot item repo

--- ProjectCOD/src/main/java/com/projectcod/repositories/HotItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projectcod.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.projectcod.models.HotItem;
-
-public interface HotItemRepository extends CrudRepository<HotItem, String> {
-	public Iterable<HotItem> findAll();
-}
